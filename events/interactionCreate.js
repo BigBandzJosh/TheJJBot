e3d8517e2403ae8756355fe3a917e3b535ca0f57
Josh <113129413+BigBandzJosh@users.noreply.github.com>
2023-05-01 11:50:15 -0300

    yip yip

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,17 +1,22 @@
 const {Events} = require('discord.js');
 
 module.exports = {
+    
     name: Events.InteractionCreate,
+    
+    // This event executes when a slash command is used.
     async execute(interaction) {
         if (!interaction.isCommand()) return;
 
         const command = interaction.client.commands.get(interaction.commandName);
-
+        // If the command doesn't exist, do nothing
         if (!command) {
         console.error(`no command found for ${interaction.commandName}`);
         return;
     }
 
+
+    //tries to execute the command, if it fails, it will send an error message
     try {
         await command.execute(interaction);
     } catch (error) {
--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,8 @@ const {Events} = require('discord.js');
 module.exports = {
     name: Events.ClientReady,
     once: true,
+
+    // This event executes when the bot is ready.
     execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
     },
