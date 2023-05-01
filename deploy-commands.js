a5d00bb50268b0de54cf3c192b522a9641cfa4b0
Josh <113129413+BigBandzJosh@users.noreply.github.com>
2023-05-01 11:32:57 -0300

    fixed deploy-commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -10,12 +10,17 @@ const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-    const filePath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(filePath).filter(file => file.endsWith('.js'));
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
     for (const file of commandFiles) {
-        const command = require(path.join(filePath, file));
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
         commands.push(command.data.toJSON());
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
     }
+}
 }
 
 const rest = new REST().setToken(process.env.DiscordAPI);
@@ -29,7 +34,7 @@ const rest = new REST().setToken(process.env.DiscordAPI);
             {body: commands},
         );
 
-        console.log(`Successfully reloaded ${commands.length} application (/) commands.}`);
+        console.log(`Successfully reloaded ${commands.length} application (/) commands.`);
     } catch (error) {
         console.error(error);
     }
