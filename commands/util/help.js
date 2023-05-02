a5ab416fdb9f9e4b74074aff4951bdc7eb411a90
Jaegar <jaegarls@icloud.com>
2023-05-02 12:06:58 -0230

    Added embed message to HELP command

--- commands/util/help.js
+++ commands/util/help.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-
+const path = require('path');
+const fs = require('fs');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('help')
@@ -7,19 +8,38 @@ module.exports = {
     async execute(interaction) {
         const helpEmbed = new EmbedBuilder()
             .setColor('#fa2f6c')
-            .setTitle('List of available commands.')
+            .setTitle('List of available slash commands.')
             .setAuthor({ name: 'TheJJBot', iconURL: 'https://i.imgur.com/AfFp7pu.png' })
             .setThumbnail('https://i.imgur.com/AfFp7pu.png')
-            .addFields(
-                { name: 'Regular field title', value: 'Some value here' },
-                { name: '\u200B', value: '\u200B' },
-                { name: 'Fun', value: 'Some value here' + '\n' + 'Some more Value', inline: true },
-                { name: 'Utility', value: 'Some value here', inline: true },
-            )
-            .addFields({ name: 'Game', value: 'Some value here', inline: true })
             .setTimestamp()
-
-            interaction.reply({ embeds: [helpEmbed] });
+        // Loop through the commands folder and the sub folders to get all the command names and add them to the embed
+        const commandFolders = fs.readdirSync(path.join(__dirname, '../')).filter(file => fs.statSync(path.join(__dirname, '../', file)).isDirectory());
+        for (const folder of commandFolders) {
+            const commandsPath = path.join(__dirname, '../', folder);
+            const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+            let commandNames = [];
+            for (const file of commandFiles) {
+                const command = require(path.join(commandsPath, file));
+                if ('data' in command && 'execute' in command) {
+                    commandNames.push(command.data.name);
+                } else {
+                    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+                }
+            }
+            helpEmbed.addFields({name: folder, value: commandNames.join('\n'), inline: true});
             
+            
+            // console.log(folder);
+            // console.log(commandNames);
+            
+        }
+        await
+
+                
+
+
+
+        interaction.reply({ embeds: [helpEmbed] });
+
     },
 };
