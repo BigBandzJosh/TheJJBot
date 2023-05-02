5d2c10ace03a7bc2d7a53bd6bdfa24bf865f981c
Josh <113129413+BigBandzJosh@users.noreply.github.com>
2023-05-02 16:20:36 -0300

    made a mistake

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,16 @@
-const {Events} = require('discord.js');
+const { Events } = require('discord.js');
 const welcome = require('./welcome.js');
 
 module.exports = {
     name: Events.ClientReady,
     once: true,
+
     // This event executes when the bot is ready.
     execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
-        // welcome(client);
+        const guild = client.guilds.cache.get(config.guildID);
+        const channel = guild.channels.cache.get(config.welcomeChannelID);
+        console.log(`Welcome messages will be sent to ${channel.name}`);
     },
 
 };
