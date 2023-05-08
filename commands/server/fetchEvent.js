354e0de794e1254aeaf28d7a46a4bde6fe05c7da
Josh <113129413+BigBandzJosh@users.noreply.github.com>
2023-05-08 13:15:07 -0300

    fetchEvent.js works! use /find along with the event name to find event

---
+++ commands/server/fetchEvent.js
@@ -0,0 +1,43 @@
+const {SlashCommandBuilder} = require('discord.js');
+const { ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle, ButtonBuilder, ButtonStyle } = require('discord.js');
+const Event = require('../../models/event.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('find')
+        .setDescription('Find your event')
+        .addStringOption(option =>
+            option.setName("eventname")
+                .setDescription("The Name of the Event")
+                .setRequired(true)),
+    async execute(interaction) {
+        try {
+            await interaction.reply(`Finding event...`);
+
+            const userEvents = await Event.findAll({
+                where: {
+                    name: global.eventTitleName = interaction.options.getString("eventname"),
+                    username: interaction.user.username,
+                    
+                },
+            });
+            if (userEvents.length > 0) {
+                await interaction.editReply(`Event found!`);
+                for (const userEvent of userEvents) {
+                    console.log(userEvent.dataValues);
+                }
+                
+            }
+            else {
+                await interaction.editReply(`Event not found!`);
+            }
+        } catch (error) {
+            console.log(error);
+            await interaction.editReply({ content: "There was an error while executing this command!", ephemeral: true });
+
+        }
+    },
+};
+
+
+
